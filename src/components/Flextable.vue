ea8780b3870f1a897bc1977eeca5d00a126db833
Łukasz Strzyżowski <lukaszstr@gmail.com>
2016-11-21 15:22:40 +0100

    Separated some functionality to components

--- src/components/Flextable.vue
+++ src/components/Flextable.vue
@@ -1,143 +1,135 @@
 <template lang="pug">
-  .flextable
-    .loader(v-if="loading")
-      flexspinner
-    .flextable-table(v-else="loading")
-      .flextable-row.header
-        .flextable-cell(v-for="cell in rows[0]") header
-      .flextable-row(v-for="row in rows")
-        .flextable-cell(v-for="cell in row") {{ cell }}
-
-    .flextable-pagination(v-if="pagination")
-      .flextable-page Page {{ page }} of {{ pages }}
-      button(v-show="page > 1" @click="setPage(page - 1)") &lt;
-      button(v-show="page < pages" @click="setPage(page + 1)") &gt;
-      select(@change="limitChanged")
-        template(v-for="limit in limits")
-          option(:value="limit") {{ limit }}
+.flextable
+  .loader(v-if="loading")
+    flexspinner
+  .flextable-table(v-else="loading")
+    .flextable-row.header
+      .flextable-cell(v-for="cell in rows[0]") header
+    .flextable-row(v-for="row in rows")
+      .flextable-cell(v-for="cell in row") {{ cell }}
+  .flextable-footer
+    flextable-paginator(v-if="pagination", :total="data.length", :size="limit")
+    flextable-select(v-model.number="limit", :options="limits")
 </template>
 
 <style lang="sass">
-  @mixin shadow
-    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)
-  @mixin border
-    border: 1px solid rgba(0,0,0,.12)
+@mixin shadow
+  box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 3px 1px -2px rgba(0,0,0,.2),0 1px 5px 0 rgba(0,0,0,.12)
+@mixin border
+  border: 1px solid rgba(0,0,0,.12)
 
-  .flextable
-    @include border
-    background: #fff
-    @include shadow
+.flextable
+  @include border
+  @include shadow
+  background: #fff
 
-  .flextable-table
+.flextable-table
+  display: flex
+  flex-direction: column
+  border:
+    collapse: collapse
+  .flextable-row
     display: flex
-    flex-direction: column
-    border:
-      collapse: collapse
-    .flextable-row
-      display: flex
-      width: auto
-      &:not(:last-child)
-        border:
-          bottom: 1px solid #c7c7c7
-      flex:
-        direction: row
-        wrap: nowrap
-      .flextable-cell
-        width: 100%
-        overflow: hidden
-        padding: 20px
+    width: auto
+    &:not(:last-child)
+      border:
+        bottom: 1px solid #c7c7c7
+    flex:
+      direction: row
+      wrap: nowrap
+    .flextable-cell
+      width: 100%
+      overflow: hidden
+      padding: 20px
 
-  .flextable-pagination
-    padding: 20px
-    text-align: right
-    .flextable-page
-      display: inline-block
-      margin:
-        right: 20px
+.header
+  font:
+    weight: bold
 
-  .header
-    font:
-      weight: bold
-  .loader
-    display: flex
-    justify-content: center
-    align-items: center
-    padding: 50px
+.loader
+  display: flex
+  justify-content: center
+  align-items: center
+  padding: 50px
 </style>
 
 <script lang="babel">
-  import Flexspinner from './Flexspinner';
+import FlextableSelect from './gui/Select';
+import FlextablePaginator from './gui/Paginator';
+import Flexspinner from './Flexspinner';
 
-  export default {
-    components: {
-      Flexspinner,
+export default {
+  components: {
+    FlextableSelect,
+    FlextablePaginator,
+    Flexspinner,
+  },
+  props: {
+    config: {
+      type: Object,
+      required: true,
     },
-    props: {
-      config: {
-        type: Object,
-        required: true,
-      },
-    },
-    data() {
-      return {
-        message: 'Flextable container',
-        resource: null,
-        data: [],
-        loading: true,
-        pagination: false,
-        limit: 10,
-        limits: [10, 20, 30, 50, 100],
-        page: 1,
-      };
-    },
-    computed: {
-      rows() {
-        const { data, pagination, page, limit } = this;
-        const offset = (page - 1) * limit;
-
-        if (pagination) {
-          return data.slice(offset, offset + limit);
-        }
-
-        return this.data;
-      },
-      pages() {
-        const { data, limit } = this;
-        return Math.ceil(data.length / limit);
-      },
-    },
-    created() {
-      const { pagination } = this.config;
+  },
+  data() {
+    return {
+      message: 'Flextable container',
+      resource: null,
+      data: [],
+      loading: true,
+      pagination: false,
+      limit: 10,
+      limits: [10, 20, 30, 50, 100],
+      page: 1,
+    };
+  },
+  computed: {
+    rows() {
+      const { data, pagination, page, limit } = this;
+      const offset = (page - 1) * limit;
 
       if (pagination) {
-        if (pagination.enabled) {
-          this.pagination = pagination.enabled;
-        }
-        if (pagination.limit) {
-          this.limit = pagination.limit;
-        }
+        return data.slice(offset, offset + limit);
       }
+
+      return this.data;
     },
-    mounted() {
-      this.data.resource = this.$resource(this.config.data.url);
-      this.getData();
+    pages() {
+      const { data, limit } = this;
+      return Math.ceil(data.length / limit);
     },
-    methods: {
-      getData() {
-        this.data.resource.get().then((response) => {
-          this.data = response.data;
-          this.loading = false;
-        });
-      },
-      setData(data) {
-        this.data = data.data;
-      },
-      limitChanged(e) {
-        this.limit = Number(e.target.value);
-      },
-      setPage(page) {
-        this.page = Math.min(Math.max(1, page), this.pages);
-      },
+  },
+  created() {
+    const { pagination } = this.config;
+
+    if (pagination) {
+      if (pagination.enabled) {
+        this.pagination = pagination.enabled;
+      }
+      if (pagination.limit) {
+        this.limit = pagination.limit;
+      }
+    }
+  },
+  mounted() {
+    this.data.resource = this.$resource(this.config.data.url);
+    this.getData();
+  },
+  methods: {
+    getData() {
+      this.data.resource.get().then((response) => {
+        this.data = response.data;
+        this.loading = false;
+      });
     },
-  };
+    setData(data) {
+      this.data = data.data;
+    },
+    limitChanged(e) {
+      this.limit = Number(e.target.value);
+    },
+    setPage(page) {
+      this.page = Math.min(Math.max(1, page), this.pages);
+    },
+  },
+};
 </script>
---
+++ src/components/gui/Paginator.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  span.flextable-paginator Page {{ page }} of {{ pages }}
+    button(v-show="page > 1" @click="nextPage") &lt;
+    button(v-show="page < pages" @click="prevPage") &gt;
+</template>
+
+<style>
+</style>
+
+<script lang="babel">
+export default {
+  props: {
+    page: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    size: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    pages() {
+      return Math.floor(this.total / this.size);
+    },
+  },
+  methods: {
+    nextPage() {
+      this.setPage(this.page + 1);
+    },
+    prevPage() {
+      this.setPage(this.page - 1);
+    },
+    setPage(page) {
+      // todo: fix this
+      this.page = page;
+    },
+  },
+};
+</script>
