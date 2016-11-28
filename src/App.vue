5e2a61e5b8dfd7085937342363251e785b42c312
Łukasz Strzyżowski <lukaszstr@gmail.com>
2016-11-28 08:27:16 +0100

    Added sorting behavior to example

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,32 @@
       :limits="[5,10,20,30,50]"
       :searchable="['id', 'firstName', 'lastName']"
       >
+      <template slot="title">
+        Users
+      </template>
       <template slot="headingRow" scope="p">
         <template v-if="p.size == 'large'">
-          <div class="ft-heading-cell ft-align-right">Id</div>
-          <div class="ft-heading-cell">First name</div>
-          <div class="ft-heading-cell">Last name</div>
+          <div class="ft-heading-cell ft-align-right ft-sortable" @click="p.sortBy('id')">
+            <template v-if="p.sort.name == 'id'">
+              <span v-if="p.sort.order == 'asc'">▲</span>
+              <span v-if="p.sort.order == 'desc'">▼</span>
+            </template>
+            <span>Id</span>
+          </div>
+          <div class="ft-heading-cell ft-sortable" @click="p.sortBy('firstName')">
+            <span>First name</span>
+            <template v-if="p.sort.name == 'firstName'">
+              <span v-if="p.sort.order == 'asc'">▲</span>
+              <span v-if="p.sort.order == 'desc'">▼</span>
+            </template>
+          </div>
+          <div class="ft-heading-cell ft-sortable" @click="p.sortBy('lastName')">
+            <span>Last name</span>
+            <template v-if="p.sort.name == 'lastName'">
+              <span v-if="p.sort.order == 'asc'">▲</span>
+              <span v-if="p.sort.order == 'desc'">▼</span>
+            </template>
+          </div>
           <div class="ft-heading-cell">E-mail</div>
           <div class="ft-heading-cell">Phone</div>
           <div class="ft-heading-cell ft-align-center">Photo</div>
@@ -57,19 +78,14 @@
             <button @click="p.delete(p.data.id)">Delete</button>
           </div>
         </template>
-        <template v-if="p.size == 'small' || p.isMobile == true">
+        <template v-if="p.size == 'small' || p.isMobile == 'true'">
           <div class="ft-cell">
-            <input type="checkbox" class="ft-accordion" />
-            <div class="ft-accordion-title" style="display: flex; justify-content: space-around">
-              <div><img :src="p.data.avatar" width="32" height="32" /></div>
-              <div>{{ p.data.lastName }} {{ p.data.firstName}}</div>
+            <input type="checkbox" class="ft-accordion">
+            <div class="ft-accordion-title">
+              {{ p.data.lastName }}
             </div>
             <div class="ft-accordion-panel">
-              <p><b>Phone:</b> {{ p.data.phone }}</p>
-              <p><b>E-mail:</b> {{ p.data.email }}</p>
-              <div class="ft-align-right">
-                <button @click="p.delete(p.data.id)">Delete</button>
-              </div>
+              {{ p.data.phone }}
             </div>
           </div>
         </template>
