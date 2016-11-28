24db311d97295d37c7319512ba33603ae22ce892
Łukasz Strzyżowski <lukaszstr@gmail.com>
2016-11-28 07:13:43 +0100

    Switched example to plain html

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,82 @@
-<template lang="pug">
-  #app
-    .header
-      h1 Flextable example
-        a(href="http://localhost:8090/users/").source  + random data rest server
+<template>
+<div id="app">
+  <div class="header">
+    <h1>Flextable example
+      <a href="" class="source"> + random date rest server</a>
+    </h1>
+  </div>
 
-    .example
-      // Usage example
-      flextable(
-        url="http://localhost:8090/users/",
-        side="server",
-        :limit.number="5",
-        :limits="[1,5,10,20,30,50,100]",
-        :searchable="['id', 'firstName', 'lastName', 'email', 'phone']",
-        )
-        template(slot="title") Users first table
-        template(slot="nodata") No users loaded
-        template(slot="headingRow" scope="p")
-          template(v-if="p.size == 'large' && p.isMobile === false")
-            .ft-heading-cell.ft-sortable.ft-align-right(@click="p.sortBy('id')")
-              span(v-if="p.sort.name == 'id' && p.sort.order == 'asc'") &#x25B2; 
-              span(v-if="p.sort.name == 'id' && p.sort.order == 'desc'") &#x25BC; 
-              span Id
-            .ft-heading-cell.ft-sortable(@click="p.sortBy('firstName')")
-              span First name
-              span(v-if="p.sort.name == 'firstName' && p.sort.order == 'asc'") &#x25B2; 
-              span(v-if="p.sort.name == 'firstName' && p.sort.order == 'desc'") &#x25BC; 
-            .ft-heading-cell.ft-sortable(@click="p.sortBy('lastName')")
-              span Last name
-              span(v-if="p.sort.name == 'lastName' && p.sort.order == 'asc'") &#x25B2; 
-              span(v-if="p.sort.name == 'lastName' && p.sort.order == 'desc'") &#x25BC; 
-            .ft-heading-cell.ft-sortable(@click="p.sortBy('email')")
-              span E-mail
-              span(v-if="p.sort.name == 'email' && p.sort.order == 'asc'") &#x25B2; 
-              span(v-if="p.sort.name == 'email' && p.sort.order == 'desc'") &#x25BC; 
-            .ft-heading-cell.ft-sortable(@click="p.sortBy('phone')")
-              span Phone
-              span(v-if="p.sort.name == 'phone' && p.sort.order == 'asc'") &#x25B2; 
-              span(v-if="p.sort.name == 'phone' && p.sort.order == 'desc'") &#x25BC; 
-            .ft-heading-cell.ft-align-center Avatar
-            .ft-heading-cell.ft-align-right Options
-          template(v-if="p.size == 'medium' && p.isMobile === false")
-            .ft-heading-cell.ft-sortable.ft-align-right Id
-            .ft-heading-cell.ft-sortable Full name
-            .ft-heading-cell.ft-sortable E-mail / Phone
-            .ft-heading-cell.ft-align-center Avatar
-            .ft-heading-cell.ft-align-right Options
-        template(slot="row" scope="p")
-          template(v-if="p.size == 'large' && p.isMobile === false")
-            .ft-cell.ft-align-right {{ p.data.id }}
-            .ft-cell {{ p.data.firstName }}
-            .ft-cell {{ p.data.lastName }}
-            .ft-cell {{ p.data.email }}
-            .ft-cell {{ p.data.phone }}
-            .ft-cell.ft-align-center
-              img(:src="p.data.avatar", width="32", height="32")
-            .ft-cell.ft-align-right
-              button(@click="p.delete(p.data.id)") &#x2718;
-          template(v-if="p.size == 'medium' && p.isMobile === false")
-            .ft-cell.ft-align-right {{ p.data.id }}
-            .ft-cell {{ p.data.firstName }} {{ p.data.lastName }}
-            .ft-cell
-              div {{ p.data.email }}
-              div {{ p.data.phone }}
-            .ft-cell.ft-align-center
-              img(:src="p.data.avatar", width="32", height="32")
-            .ft-cell.ft-align-right
-              button(@click="remove(p.data.id)") {{ p.data.id }}
-          template(v-if="p.size == 'small' || p.isMobile == true")
-            .ft-cell
-              input(type="checkbox").ft-accordion
-              .ft-accordion-title(style="display: flex; justify-content: space-around")
-                div(style="width: 100%").ft-align-center
-                  img(:src="p.data.avatar", width="32", height="32")
-                div(style="padding: 8px; width: 100%").ft-align-left {{ p.data.firstName }} {{ p.data.lastName }}
-              .ft-accordion-panel
-                p E-mail: {{ p.data.email }}
-                p Phone: {{ p.data.phone }}
-                .ft-align-right
-                  button(@click="remove(p.data.id)") {{ p.data.id }}
+  <div class="example">
+    <flextable
+      url="http://localhost:8090/users/"
+      side="client"
+      :limit.number="5"
+      :limits="[5,10,20,30,50]"
+      :searchable="['id', 'firstName', 'lastName']"
+      >
+      <template slot="headingRow" scope="p">
+        <template v-if="p.size == 'large'">
+          <div class="ft-heading-cell ft-align-right">Id</div>
+          <div class="ft-heading-cell">First name</div>
+          <div class="ft-heading-cell">Last name</div>
+          <div class="ft-heading-cell">E-mail</div>
+          <div class="ft-heading-cell">Phone</div>
+          <div class="ft-heading-cell ft-align-center">Photo</div>
+          <div class="ft-heading-cell ft-align-right">Options</div>
+        </template>
+        <template v-if="p.size == 'medium'">
+          <div class="ft-heading-cell ft-align-right">Id</div>
+          <div class="ft-heading-cell">Full name</div>
+          <div class="ft-heading-cell">E-mail / Phone</div>
+          <div class="ft-heading-cell ft-align-center">Photo</div>
+          <div class="ft-heading-cell ft-align-right">Options</div>
+        </template>
+      </template>
+      <template slot="row" scope="p">
+        <template v-if="p.size == 'large'">
+          <div class="ft-cell ft-align-right">{{ p.data.id }}</div>
+          <div class="ft-cell">{{ p.data.firstName }}</div>
+          <div class="ft-cell">{{ p.data.lastName }}</div>
+          <div class="ft-cell">{{ p.data.email }}</div>
+          <div class="ft-cell">{{ p.data.phone }}</div>
+          <div class="ft-cell ft-align-center">
+            <img :src="p.data.avatar" width="32" height="32" />
+          </div>
+          <div class="ft-cell ft-align-right">
+            <button @click="p.delete(p.data.id)">Delete</button>
+          </div>
+        </template>
+        <template v-if="p.size == 'medium'">
+          <div class="ft-cell ft-align-right">{{ p.data.id }}</div>
+          <div class="ft-cell">{{ p.data.lastName }} {{ p.data.firstName }}</div>
+          <div class="ft-cell">{{ p.data.email }}<br />{{ p.data.phone }}</div>
+          <div class="ft-cell ft-align-center">
+            <img :src="p.data.avatar" width="32" height="32" />
+          </div>
+          <div class="ft-cell ft-align-right">
+            <button @click="p.delete(p.data.id)">Delete</button>
+          </div>
+        </template>
+        <template v-if="p.size == 'small' || p.isMobile == true">
+          <div class="ft-cell">
+            <input type="checkbox" class="ft-accordion" />
+            <div class="ft-accordion-title" style="display: flex; justify-content: space-around">
+              <div><img :src="p.data.avatar" width="32" height="32" /></div>
+              <div>{{ p.data.lastName }} {{ p.data.firstName}}</div>
+            </div>
+            <div class="ft-accordion-panel">
+              <p><b>Phone:</b> {{ p.data.phone }}</p>
+              <p><b>E-mail:</b> {{ p.data.email }}</p>
+              <div class="ft-align-right">
+                <button @click="p.delete(p.data.id)">Delete</button>
+              </div>
+            </div>
+          </div>
+        </template>
+      </template>
+    </flextable>
+  </div>
+</div>
 </template>
 
 <style lang="sass">
